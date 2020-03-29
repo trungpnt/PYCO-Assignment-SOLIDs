b5e990acbb34f0a973d693a73f01f7db6b8067c3
Rayst <trungpnt0605@gmail.com>
2020-03-29 11:31:09 +0700

    Renamed objects' names to meaningful ones

--- O-Principle/O-Principle-CONFORMED/src/Client/EmergencyProcess.java
+++ O-Principle/O-Principle-CONFORMED/src/Client/EmergencyProcess.java
@@ -7,10 +7,10 @@ import HospitalStaff.SecurityGuard;
 public class EmergencyProcess {
     public static void main(String [] args){
         HospitalManagement someDirector = new HospitalManagement();
-        COVID19Fighter security_1 = new SecurityGuard("Uncle Bob",1,true);
-        someDirector.callUpon(security_1);
+        COVID19Fighter Bobby = new SecurityGuard("Uncle Bob",1,true);
+        someDirector.callUpon(Bobby);
 
-        COVID19Fighter security_2 = new SecurityGuard("Aunt Mary",2,true);
-        someDirector.callUpon(security_2);
+        COVID19Fighter Morgana = new SecurityGuard("Aunt Morgana",2,true);
+        someDirector.callUpon(Morgana);
     }
 }
